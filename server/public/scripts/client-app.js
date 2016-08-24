fb94c457867b8c9f775017c834de3d5025b411e8
kmaimn <kmai.mn@gmail.com>
2016-08-23 22:32:40 -0500

    updates

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -4,21 +4,22 @@ myApp.controller('OrdersController', ['$scope', '$http', function ($scope, $http
 
   console.log('IndexController loaded')
 
+  var id = $(this).parent().data('id');
+
   $http({
     method: 'GET',
-    url: '/orders'
+    url: '/orders/names'
   }).then(function (response){
     console.log('response object ', response);
     $scope.customers = response.data;
   });
 
-
-  // $http({
-  //   method: 'GET',
-  //   url: '/orders' + id,
-  // }).then(function (response) {
-  //   console.log('response object ', response);
-  //   $scope.orders = response.data();
-  // });
+  $http({
+    method: 'GET',
+    url: '/orders' + id,
+  }).then(function (response) {
+    console.log('response object ', response);
+    $scope.orders = response.data();
+  });
 
 }]);
--- server/routes/order.js
+++ server/routes/order.js
@@ -3,14 +3,16 @@ var router = express.Router();
 var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/omicron';
 
-router.get('/', function(req, res){
+router.get('/names', function(req, res){
 
   pg.connect(connectionString, function (err, client, done){
     if (err){
       res.sendStatus(500);
     }
 
-    client.query('SELECT * FROM customers JOIN addresses ON customer_id = customers.id LEFT JOIN orders ON address_id = addresses.id', function (err, result){
+    client.query('SELECT COUNT (orders.id), customers.first_name, customers.last_name FROM orders ' + 'RIGHT JOIN addresses ON addresses.id = orders.address_id ' +
+    'JOIN customers ON customers.id = addresses.customer_id ' +
+    'GROUP BY customers.id;', function (err, result){
       console.log("post request");
       done();
 
@@ -23,8 +25,9 @@ router.get('/', function(req, res){
   });
 });
 
-
-router.get('/', function(req, res){
+router.get('/orders:id', function(req, res){
+  var id = req.params.id;
+  console.log(id);
   pg.connect(connectionString), function (err, client, done){
     if (err){
       res.sendStatus(500);
